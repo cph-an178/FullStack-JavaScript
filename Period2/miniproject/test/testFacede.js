71f194f453f1af28965705c1cd0f95c528cfe479
Alexander Nilsen <cph-an178@cphbusiness.dk>
2018-03-22 15:42:16 +0100

    Update on testFacade.js

--- Period2/miniproject/test/testFacede.js
+++ Period2/miniproject/test/testFacede.js
@@ -3,6 +3,10 @@ var facade = require("../facades/dbFacade")
 
 const expect = require("chai").expect;
 
+var testUser = function(firstName, lastName, userName, password) {
+    facade.addUser(firstName, lastName, userName, password)
+}
+
 describe("Test of addUser", function(){
     it("Add user to mLab mongoDB and return user promise", function() {
         expect(facade.addUser("Kim","Bo","kim_bo","password")).to.be.equal
