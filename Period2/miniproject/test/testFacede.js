9f665c39bf7fc9a441798fe4a1fd2fa9e7373239
Alexander Nilsen <cph-an178@cphbusiness.dk>
2018-04-01 15:38:05 +0200

    Added test for addUser(Done) and addJob(WIP)

--- Period2/miniproject/test/testFacede.js
+++ Period2/miniproject/test/testFacede.js
@@ -1,14 +1,44 @@
 require("../dbSetup.js");
-var facade = require("../facades/dbFacade")
-
 const expect = require("chai").expect;
+var facade = require("../facades/dbFacade")
+var User = require("../models/user")
 
-var testUser = function(firstName, lastName, userName, password) {
-    facade.addUser(firstName, lastName, userName, password)
-}
 
-describe("Test of addUser", function(){
-    it("Add user to mLab mongoDB and return user promise", function() {
-        expect(facade.addUser("Kim","Bo","kim_bo","password")).to.be.equal
+// Clean db 
+before(async function() {
+    await User.remove({});
+});
+
+describe("Testing dbFacade", function(){
+    describe("Add two new Users",function(){
+        it("Should add Kurt Wonnegut and Bo Lemmingsen", async function(){
+            var userPromises = [
+                facade.addUser("Kurt", "Wonnegut", "kw", "passw0rd"),
+                facade.addUser("Bo", "Lemmingsen", "bl", "654321")
+            ];
+            var users = await Promise.all(userPromises);
+            // Getting all users
+            var getUsers = await User.find({}).exec();
+
+            expect(getUsers.length).to.be.equal(2);
+            // Used to check id in next test
+            console.log("Kurts id:" + users[0]._id)
+        });
+    });
+    describe("Add job to user", function(){
+        it("Should add Blogger at Time to Kurts job", async function(){
+            // Get user Kurt
+            var kurt = await User.findOne({userName: "kw"}).exec();
+            // See if we get the correct id
+            console.log("Kurts id:" + kurt._id);
+            // Function which adds a job to the mongoDB
+            await facade.addJobToUser(kurt._id, "Blogger", "Time", "Time.com");
+            // See if we can get the type
+            console.log(kurt.job.type); 
+            // For some reason i cant... 
+
+            //expect(kurt.job.type).to.be.equal("Blogger");
+
+        })
     })
-})
+});
