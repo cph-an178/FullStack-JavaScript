40c73d5a4b8c25dcfbbebdc1e4928b044672d775
Alexander Nilsen <cph-an178@cphbusiness.dk>
2018-05-16 10:01:15 +0200

    Completed Mutianon exervice

--- Period4/apollo-graphql/src/index.js
+++ Period4/apollo-graphql/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Queries from './Queries';
+import Mutations from './Mutations';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Queries />, document.getElementById('root'));
+ReactDOM.render(<Mutations />, document.getElementById('root'));
 registerServiceWorker();
